fc7557e50c4de7008ea388227af2183f57183adf
Rishabh Arora <rishabhjecrcu@gmail.com>
2017-04-07 00:42:22 +0530

    Second section cart modal pay modal

--- js/index.js
+++ js/index.js
@@ -1,28 +1,47 @@
-$(document).ready(function(){
-	$.ajax({
-	url:"http://acadprojects.com/py/fabricKart/sort/items",
-	type:'GET',
-	data: {
-	"sort_by": "relevance",
-	"page": 0
-	},
-	success: function(response){
-	console.log(response);
-	var data=response["data"];
-	data.forEach(function(product,index){
-	var productName=product["item_name"];
-	var image=product["image"];
-	var brand=product["brand"];
-	var price=product["price"];
+function getProductList(pgno) {
+	$.ajax({ 
+		url:"http://acadprojects.com/py/fabricKart/sort/items",
+		type:'GET',
+		data: {
+			"sort_by": "relevance",
+			"page": pgno
+		},
+		success: function(response){
+			$('.product-container-parent').empty();
+			console.log(response);
+			var data = response["data"];
+			data.forEach(function(product,index)
+			{
+				var productName = product["item_name"];
+				var image = product["image"];
+				var brand = product["brand"];
+				var price = product["price"];
 
-	var productNameTag= $('<div class="productName "></div>').html(productName);
-	var imageTag= $('<img width="150px" height= "150px">').attr("src",image);
-	var brandTag= $('<div class="brand"></div>').html(brand);
-	var priceTag= $('<div class="price"></div>').html(price);
-	var productContainer= $('<div class="product-container col-md-3 col-md-offset-1 "></div>').append(imageTag,productNameTag,brandTag,priceTag);
-	$('.container').append(productContainer);
+				var productNameTag = $('<div class="product-name "></div>').html(productName);
+				var imageTag = $('<img class ="imgg"width="150px" height= "200px">').attr("src",image);
+				var brandTag = $('<div class="brand"></div>').html(brand);
+				var priceTag = $('<div class="price"></div>').html(price);
+				var cart = $('<img class="small-cart" height ="20px" width="20px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Shopping_cart_icon.svg/2000px-Shopping_cart_icon.svg.png">')
+				var productContainer = $('<div class="product-container col-md-3 col-md-offset-1 "></div>').append(imageTag,productNameTag,brandTag,priceTag,cart);
+				$('.product-container-parent').append(productContainer);
 
+			});
+		}
 	});
-	}
+
+}
+
+$(document).ready(function(){
+	getProductList(0);
+	$('.pagination li').click(function () {
+		var pgno = parseInt($(this).text());
+		getProductList(pgno-1);
 	});
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
